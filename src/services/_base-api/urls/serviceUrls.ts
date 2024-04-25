1a3c8457bae3826eaa6080a64e61ddb4b8a32a78
revan <klayton.alan@gmail.com>
2024-04-25 08:10:11 +0500

    dist

--- src/services/_base-api/urls/serviceUrls.ts
+++
@@ -1,9 +0,0 @@
-export enum ApiService {
-  BINANCE_SERVICE = 'BINANCE_SERVICE',
-  BINANCE_STREAM = 'BINANCE_STREAM'
-}
-
-export const apiServiceUrls: Record<ApiService, string> = {
-  BINANCE_SERVICE: 'https://api.binance.com/api/v3',
-  BINANCE_STREAM: 'wss://stream.binance.com:9443/ws',
-}
